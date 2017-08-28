66223e64449e681cd683482c7320643fb9194754
Kwon Sungbong <sb.kwon@anypointmedia.com>
2017-08-28 16:51:23 +0900

    test

--- src/main/java/com/example/batch/config/SchedulingConfiguration.java
+++ src/main/java/com/example/batch/config/SchedulingConfiguration.java
@@ -1,5 +1,9 @@
 package com.example.batch.config;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,9 +15,19 @@ import org.springframework.scheduling.annotation.Scheduled;
 @EnableScheduling
 public class SchedulingConfiguration {
 
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job job;
+
     @Scheduled(fixedRate = 5000)
-    public void test() {
-        String test = "test";
+    public void test() throws Exception {
+        handle();
+    }
+
+    public void handle() throws Exception {
+        jobLauncher.run(job, new JobParameters());
     }
 
 }
