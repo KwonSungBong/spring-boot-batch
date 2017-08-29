c4905ab2e7fd4279ddd8c14f840013918f792dbc
Kwon Sungbong <sb.kwon@anypointmedia.com>
2017-08-29 14:54:04 +0900

    test

--- src/main/java/com/example/batch/config/SchedulingConfiguration.java
+++ src/main/java/com/example/batch/config/SchedulingConfiguration.java
@@ -1,12 +1,16 @@
 package com.example.batch.config;
 
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ScheduledExecutorFactoryBean;
+
+import java.util.Date;
 
 /**
  * Created by whilemouse on 17. 8. 28.
@@ -14,6 +18,13 @@ import org.springframework.scheduling.annotation.Scheduled;
 @Configuration
 @EnableScheduling
 public class SchedulingConfiguration {
+
+    @Bean
+    public ScheduledExecutorFactoryBean scheduledExecutorService() {
+        ScheduledExecutorFactoryBean bean = new ScheduledExecutorFactoryBean();
+        bean.setPoolSize(5);
+        return bean;
+    }
 
     @Autowired
     private JobLauncher jobLauncher;
@@ -23,11 +34,15 @@ public class SchedulingConfiguration {
 
     @Scheduled(fixedRate = 5000)
     public void test() throws Exception {
-//        handle();
+        handle();
     }
 
     public void handle() throws Exception {
-        jobLauncher.run(job, new JobParameters());
+        JobParametersBuilder builder = new JobParametersBuilder();
+        builder.addString("time", String.valueOf(System.currentTimeMillis()));
+        builder.addDate("date", new Date());
+        jobLauncher.run(job, builder.toJobParameters());
+//        jobLauncher.run(job, new JobParameters());
     }
 
 }
